f0fbd0950f18174c210f85b5cae06080322ac9d1
admin <admin>
2016-10-10 15:14:06 -0400

    added gitignore

---
+++ Assets/HoloToolkit/Utilities/Scripts/GazeGestureManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+public class GazeGestureManager : MonoBehaviour {
+
+	// Use this for initialization
+    public static GazeGestureManager Instance { get; private set; }
+    // selected object
+    public GameObject FocusedObject { get; private set; }
+
+    GestureRecognizer recognizer;
+
+    // initialization
+    void Awake()
+    {
+        Instance = this;
+
+        //setting up Gesture Recognizer
+        recognizer = new GestureRecognizer();
+        recognizer.TappedEvent += (source, tapCount, ray) =>
+         {
+             //send OnSelect message to the focused object
+             if(FocusedObject != null)
+             {
+                 FocusedObject.SendMessageUpwards("OnSelect");
+             }
+         };
+        recognizer.StartCapturingGestures();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        GameObject oldFocusObject = FocusedObject;
+
+        //Do a raycast into the world based on the suers head position
+
+        var headPosition = Camera.main.transform.position;
+        var gazeDirection = Camera.main.transform.forward;
+
+        RaycastHit hitInfo;
+        if(Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+        {
+            //if the raycast hit a hologram use that as focus object
+            FocusedObject = hitInfo.collider.gameObject;
+        }
+        else
+        {
+            //if raycast didn't hit anything clear object
+            FocusedObject = null;
+        }
+        //new object is selected
+        if (FocusedObject != oldFocusObject)
+        {
+            recognizer.CancelGestures();
+            recognizer.StartCapturingGestures();
+        }
+	}
+}
---
+++ Assets/HoloToolkit/Utilities/Scripts/ObjectCommands.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ObjectCommands : MonoBehaviour {
+    
+    public bool isSelected;
+    private static float radian = 0.0174533f;
+	// Called by GazeGestureManager when the user performs a Select gesture
+    void OnSelect()
+    {
+        if (!this.isSelected)
+        {
+            this.isSelected = true;
+        }
+        /**if (!this.GetComponent<Rigidbody>())
+        {
+            var rigidbody = this.gameObject.AddComponent<Rigidbody>();
+            rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        }*/
+    }
+    void Update()
+    {
+
+        
+        if (this.isSelected)
+        {
+            var gazeDirection = Camera.main.transform.forward;
+            var headPosition = Camera.main.transform.position;
+            var headRotation = Camera.main.transform.eulerAngles;
+            float x = (float)Math.Sin(System.Convert.ToDouble(headRotation.y.ToString()) * radian);
+            float y = -(float)Math.Tan(System.Convert.ToDouble(headRotation.x.ToString()) * radian);
+            float z = (float)Math.Cos(System.Convert.ToDouble(headRotation.y.ToString()) * radian);
+            y -= (float)Math.Cos(System.Convert.ToDouble(headRotation.x.ToString()) * radian);
+            var newPosition = headPosition + new Vector3(x * 2, y*2, z * 2);
+            this.gameObject.transform.position = newPosition;
+            this.gameObject.transform.eulerAngles = headRotation;
+            Debug.Log("new position " + newPosition);
+            Debug.Log("new rotation " + headRotation);
+           
+
+        }
+    }
+  
+
+    
+}
