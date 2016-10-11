691f76c946e24dc567fa667304104d2ab41c3cca
Prem Stephen <Prem.Stephen@isobar.com>
2016-10-11 15:33:56 -0400

    car moves, but not stopping in front of my eyes

---
+++ Assets/HoloToolkit/Utilities/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+
+
+class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Debug.Log("prem " + moveHorizontal + " : " + moveVertical);
+
+        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Vector3 movement = new Vector3(0.0f, 0.0f, 2.0f);
+
+        rb.AddForce(movement * speed);
+    }
+}
+
