7a78585f25d6d21e43e3311025883428c6827375
admin <admin>
2016-10-11 14:11:39 -0400

    adding deselection

--- Assets/HoloToolkit/Utilities/Scripts/ObjectCommands.cs
+++ Assets/HoloToolkit/Utilities/Scripts/ObjectCommands.cs
@@ -10,9 +10,13 @@ public class ObjectCommands : MonoBehaviour {
 	// Called by GazeGestureManager when the user performs a Select gesture
     void OnSelect()
     {
+        Debug.Log("ON_SELECT");
         if (!this.isSelected)
         {
             this.isSelected = true;
+        }else
+        {
+            this.isSelected = false;
         }
         /**if (!this.GetComponent<Rigidbody>())
         {
@@ -30,16 +34,18 @@ public class ObjectCommands : MonoBehaviour {
             var headPosition = Camera.main.transform.position;
             var headRotation = Camera.main.transform.eulerAngles;
             float x = (float)Math.Sin(System.Convert.ToDouble(headRotation.y.ToString()) * radian);
-            float y = -(float)Math.Tan(System.Convert.ToDouble(headRotation.x.ToString()) * radian);
+            float y = -(float)Math.Sin((System.Convert.ToDouble(headRotation.x.ToString())) * radian);
             float z = (float)Math.Cos(System.Convert.ToDouble(headRotation.y.ToString()) * radian);
-            z *= (float)Math.Cos(System.Convert.ToDouble(headRotation.x.ToString()) * radian);
-            //z = (float)(1 - Math.Pow((double)x,2) - Math.Pow((double)y,2));
-            //z = (float)Math.Sqrt(z);
+
+           // x *= (float)Math.Cos((System.Convert.ToDouble(headRotation.x.ToString())) * radian);
+           // z *= (float)Math.Cos((System.Convert.ToDouble(headRotation.x.ToString())) * radian);
+
+
+
             var newPosition = headPosition + new Vector3(x * 2, y*2, z*2);
             this.gameObject.transform.position = newPosition;
             this.gameObject.transform.eulerAngles = headRotation;
-            Debug.Log("new position " + newPosition);
-            Debug.Log("new rotation " + headRotation);
+          
            
 
         }
