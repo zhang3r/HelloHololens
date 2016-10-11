396d9c9d281df4a1c31479192c665d509790e592
Prem Stephen <Prem.Stephen@isobar.com>
2016-10-11 15:42:50 -0400

    iterations don't work, neither does a very low speed, WIP

--- Assets/HoloToolkit/Utilities/Scripts/PlayerController.cs
+++ Assets/HoloToolkit/Utilities/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ using System.Collections;
 class PlayerController : MonoBehaviour
 {
     public float speed;
+    public Int32 count;
 
     private Rigidbody rb;
 
@@ -19,14 +20,18 @@ class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Debug.Log("prem " + moveHorizontal + " : " + moveVertical);
+        if (count > 0)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Debug.Log("prem " + moveHorizontal + " : " + moveVertical);
 
-        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        Vector3 movement = new Vector3(0.0f, 0.0f, 2.0f);
+            //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            Vector3 movement = new Vector3(0.0f, 0.0f, 2.0f);
 
-        rb.AddForce(movement * speed);
+            rb.AddForce(movement * speed);
+            count--;
+        }
     }
 }
 
